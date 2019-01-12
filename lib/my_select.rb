05229b376f77dd34ca0a819c3e0085ed5117faf3
Samidh Patel <spatel13456@gmail.com>
2019-01-12 02:29:59 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   my_selection = []
   while i < collection.length
-    my_selection << yield(collection[i])
+    if yield(collection[i])
     i += 1
   end 
   my_selection
